5cc32c49181734528d9532dc8ab7df501c0fc6dd
fined-nsu <71229695+fined-nsu@users.noreply.github.com>
2021-03-07 15:51:33 -0600

    Tutorial Complete

---
+++ FallingWordsTyping/Assets/WordGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordGenerator : MonoBehaviour
+{
+    private static string[] wordList = { "lizard", "dragon" };
+
+    public static string GetRandomWord()
+    {
+        int randomIndex = Random.Range(0, wordList.Length);
+        string randomWord = wordList[randomIndex];
+
+        return randomWord;
+    }
+}
